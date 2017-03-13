982ca7dd9127ecea257c530b1880ed2bf24123f0
Jordan Holliday <jordan.holliday@tapad.com>
2017-03-13 09:12:49 -0400

    update nav, show all picks on config page

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -2,6 +2,6 @@ class ConfigsController < ApplicationController
     before_action :redirect_unless_admin
     
     def index
-        
+        @picks = Pick.includes(:round, :team, :user).all.sort_by(&:entry)
     end
 end
