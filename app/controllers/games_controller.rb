4475494ad09b781f74f8b34e49f8542089b907a6
Jordan Holliday <jordan.holliday@tapad.com>
2017-03-18 08:34:04 -0400

    bring back new games form, hope it works in prod

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,15 +2,23 @@ class GamesController < ApplicationController
     before_action :redirect_unless_admin
 
     def index 
-        @teams = Team.all.sort_by {|team| [team.region, team.seed] }
-        @games = Game.includes(:results, :home_team, :away_team).all
-        @rounds = Round.all.sort
+        @games = Game
+            .includes(:results, :home_team, :away_team)
+            .all
+            .sort_by { |game| -game.round_id }
+
+
+        losses_team_ids = Result.all.select { |result| result.loss? }.map(&:team_id)
+        @teams = Team.all
+            .select { |team| !losses_team_ids.include?(team.id) }
+            .sort_by { |team| team.seed }
+        @rounds = [Round.second]
         @tournaments = Tournament.all
-        @game = Game.new
     end
 
     def create
         @game = Game.new(game_params)
+        @game.tournament_id = Tournament.first.id
 
         if @game.save
             redirect_to games_url
